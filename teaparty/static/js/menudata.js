6d5055274ac35e8cbddd9398ce5c6fd24eab986f
Mouse Reeve <mousereeve@riseup.net>
2016-06-29 22:27:13 -0700

    Forgot to add all the data

---
+++ teaparty/static/js/menudata.js
@@ -0,0 +1,81 @@
+var menu = [
+    {
+        count: 4,
+        title: "Tea Selection",
+        data: [
+            {name: "Darjeeling"},
+            {name: "Rose Petal Black"},
+            {name: "Blackcurrant"},
+            {name: "Rooibos"},
+            {name: "Keemun"},
+            {name: "Earl Grey"},
+            {name: "Peppermint"}
+        ]
+    }, {
+        count: 2,
+        title: "Savories",
+        data: [
+            {name: "Some Bullshit Salad",
+             content: "Argula I guess?"},
+            {name: "Asparagus Mini Quiche",
+             content: "Creamy walnut- and navy bean-based vegan quiche"},
+            {name: "Cauliflower soup",
+             content: "Small cup of silky cauliflower soup"},
+            {name: "Spring Rolls",
+             content: "Vegatables and tofu wrapped in rice paper"},
+        ]
+    }, {
+        count: 3,
+        title: "Sandwiches",
+        data: [
+            {name: "Cucumber Cream Cheese",
+             content: "Vegan cream cheese with sliced cucumber and dill on white bread"},
+            {name: "Avocado Toast",
+             content: "Sliced avocado on toasted walnut bread, topped with salt and cracked pepper"},
+            {name: "Cheddar & Pickle",
+             content: "Miyoko's Rustic Alpine cashew cheese and pickles on toasted sourdough bread"},
+            {name: "Cucumber & Avocado",
+             content: "Cucumber and sliced avocado with dill and mint on white bread"},
+            {name: "Watercress and Butter",
+             content: "Vegan butter and watercress on white bread"},
+            {name: "Sweet Onion & Hummus",
+             content: "Sautéed red onion and hummus on sourdough bread"},
+        ]
+    }, {
+        count: 1,
+        title: "Crumpets",
+        data: [
+            {name: "with Lemon Curd, Jam, and Cashew Cream",
+             recipe: "http://blog.kingarthurflour.com/2009/09/18/butters-best-friend-crumpets/"}
+        ]
+    }, {
+        count: 2,
+        title: "Scones",
+        data: [
+            {name: "Lemon Ginger",
+             content: "Lemon scone with candied ginger",
+             recipe: "http://www.joyofbaking.com/SconesLemonGinger.html"},
+            {name: "Raspberry Jam",
+             content: "Cream scone with fruit preserves",
+             recipe: "http://www.joyofbaking.com/SconesfilledwitheJam.html"},
+            {name: "Pumpkin",
+             content: "Pumpkin scone with walnuts or pecans",
+             recipe: "http://www.joyofbaking.com/printpages/PumpkinSconesprint.html"},
+        ]
+    }, {
+        count: 3,
+        title: "Sweets",
+        data: [
+            {name: "Rosemary Shortbread Cookies",
+             recipe: "https://github.com/mouse-reeve/recipes/blob/master/markdown/rosemary_shortbread.md"},
+            {name: "Peach Tartlettes"},
+            {name: "Pistachio Rosewater Cookies",
+             recipe: "http://www.80cakes.com/2014/07/vegan-rose-pistachio-cookies/"},
+            {name: "Gingerbread Cookies"},
+            {name: "Jam Tarts",
+             recipe: "http://www.joyofbaking.com/JamTarts.html"},
+            {name: "Mini Cupcakes",
+             recipe: "http://www.theppk.com/2010/06/2000/"},
+        ]
+    },
+];
